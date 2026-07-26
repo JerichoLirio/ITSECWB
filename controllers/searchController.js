a8efbd4902afa5d376e68ba11cda7c8e68cf9536
uhlekkk <john_alec_flores@dlsu.edu.ph>
2026-07-26 17:02:20 +0800

    Delete controllers directory

--- controllers/searchController.js
+++
@@ -1,6 +0,0 @@
-exports.search = (req, res) => {
-  const query = (req.query.q || '').toUpperCase();
-  const labs = ['G404A', 'G404B', 'V101'];
-  const results = labs.filter(lab => lab.startsWith(query));
-  res.json({ success: true, results });
-};
